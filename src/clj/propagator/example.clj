500536d408c91207983d65b47eee0f26637b8001
Michael Nygard <michael.nygard@n6consulting.com>
2011-06-13 13:58:31 -0500

    remove incomplete example

--- src/clj/propagator/example.clj
+++ src/clj/propagator/example.clj
@@ -1,13 +1,4 @@
 (ns propagator.example
   (:use propagator.core))
 
-(defn heron-step [x g h]
-  (compound-propagator [x g]
-                       (fn []
-                         (let [x-div-g (make-cell)
-                               g+x-div-g (make-cell)
-                               two (make-cell)]
-                           (divider x g x-div-g)
-                           (adder g x-div-g g+x-div-g)
-                           ((constant 2) two)
-                           (divider g+x-div-g two h)))))
+
